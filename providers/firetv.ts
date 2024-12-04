aeaa8a7b8c3fcd9a679dae5d10d9351991c6cedc
spacefall <lucarichu@outlook.it>
2024-12-04 22:31:29 +0100

    perf(firetv): using project json instead of downloading one every time

--- providers/firetv.ts
+++ providers/firetv.ts
@@ -1,5 +1,6 @@
-import { getJson, randInt } from "../src/utils";
 import type { FinalJson } from "../src/types";
+import { getJson, randInt } from "../src/utils";
+import ftvData from "./firetv_data.json";
 
 // json format of the json file (in the gist)
 export interface FTVJson {
@@ -10,13 +11,12 @@ export interface FTVJson {
 }
 
 // url of the json containing the firetv wallpapers
-const url =
-	"https://gist.githubusercontent.com/spacefall/0cc095656f67e826977c84eecdd89b3c/raw/07553644e25c653fc099aa3e1058456b82c73d6f/firetv.json";
+//const url = "https://gist.githubusercontent.com/spacefall/0cc095656f67e826977c84eecdd89b3c/raw/07553644e25c653fc099aa3e1058456b82c73d6f/firetv.json";
 
 // Grabs a list of wallpapers used on the FireTV screensaver and returns one
 // noinspection JSUnusedGlobalSymbols
 export async function provide(): Promise<FinalJson> {
-	const json = (await getJson(url)) as FTVJson[];
+	const json = ftvData as FTVJson[];
 	const chosenOne = json[randInt(json.length)];
 
 	// json build
