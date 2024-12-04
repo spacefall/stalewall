5e355482bff0a043d9bfa1b82e5a41a9da525cbf
spacefall <lucarichu@outlook.it>
2024-12-04 23:16:45 +0100

    refactor(providerList): removed unused imports

--- src/providerList.ts
+++ src/providerList.ts
@@ -2,7 +2,7 @@ import * as bing from "../providers/bing";
 import * as chrcast from "../providers/chromecast";
 import * as ftv from "../providers/firetv";
 import * as spot from "../providers/spotlight";
-import type { FinalJson, ProviderList, Settings } from "./types";
+import type { ProviderList } from "./types";
 
 // This file exists to import all providers and put them in a list, this would be done at runtime (see devLoadProviders)
 // but unfortunately Cloudflare Workers doesn't support fs so they have to be shoved in a list manually
