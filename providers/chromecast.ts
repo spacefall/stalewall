6403992744edec2c620fd777e7905a19f4ce0df7
spacefall <lucarichu@outlook.it>
2024-12-04 23:18:38 +0100

    feat: inital proxy implementation
    Mostly a test and only includes chromecast.ts as it's more of a test for the proxy than a feature at the moment

--- providers/chromecast.ts
+++ providers/chromecast.ts
@@ -1,12 +1,12 @@
 import * as cheerio from "cheerio";
-import type { FinalJson } from "../src/types";
-import { getText, randInt } from "../src/utils";
+import type { FinalJson, Settings } from "../src/types";
+import { getProxyUrl, getText, randInt } from "../src/utils";
 
 // url of the chromecast homepage (contains a json with 50 wallpapers)
 const url = "https://google.com/cast/chromecast/home/";
 
 // Grabs a list of wallpapers from the Chromecast homepage and returns one
-export async function provide(): Promise<FinalJson> {
+export async function provide(set: Settings): Promise<FinalJson> {
 	// loads the homepage and extracts the text from the only script tag in the body (which is regenerated on every request)
 	const homepage = await getText(url);
 	const $ = cheerio.load(homepage);
@@ -28,7 +28,7 @@ export async function provide(): Promise<FinalJson> {
 
 	const chosenOne = json[0][randInt(50)];
 
-	return {
+	const finalJson: FinalJson = {
 		provider: "chromecast",
 		url: `${chosenOne[0].before("=")}=w0`,
 		info: {
@@ -40,4 +40,39 @@ export async function provide(): Promise<FinalJson> {
 			},
 		},
 	};
+
+	if (set.proxy) {
+		finalJson.url = proxy(finalJson.url, set.width, set.height);
+	}
+	return finalJson;
+}
+
+function proxy(img: string, width?: number, height?: number): string {
+	const proxyURL = getProxyUrl();
+	const imgURL = new URL(img);
+
+	// setting provider
+	proxyURL.searchParams.set("prov", "chromecast");
+	// specifying if original url is /proxy/ or /chromecast-private-photos/
+	switch (imgURL.pathname.charAt(1)) {
+		case "c":
+			proxyURL.searchParams.set("type", "pp");
+			proxyURL.searchParams.set("id", imgURL.pathname.after("photos/").before("="));
+			break;
+
+		case "p":
+			proxyURL.searchParams.set("type", "pr");
+			proxyURL.searchParams.set("id", imgURL.pathname.after("proxy/").before("="));
+			break;
+
+		default:
+			throw new Error("Invalid chromecast image url");
+	}
+
+	if (height && width) {
+		proxyURL.searchParams.set("h", height.toString());
+		proxyURL.searchParams.set("w", width.toString());
+	}
+
+	return proxyURL.toString();
 }
