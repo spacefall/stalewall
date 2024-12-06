063e5f6116e8f9741945564de9d3886ab6288738
spacefall <lucarichu@outlook.it>
2024-12-06 22:24:37 +0100

    refactor: made it kinda easier (probably just neater) to add providers to list

--- src/providerList.ts
+++ src/providerList.ts
@@ -1,5 +1,6 @@
 import * as bing from "../providers/bing";
 import * as chrcast from "../providers/chromecast";
+import * as earth from "../providers/earthview";
 import * as ftv from "../providers/firetv";
 import * as spot from "../providers/spotlight";
 import type { ProviderList } from "./types";
@@ -7,4 +8,4 @@ import type { ProviderList } from "./types";
 // This file exists to import all providers and put them in a list, this would be done at runtime (see devLoadProviders)
 // but unfortunately Cloudflare Workers doesn't support fs so they have to be shoved in a list manually
 
-export const providers: ProviderList = [bing.provide, ftv.provide, chrcast.provide, spot.provide];
+export const providers: ProviderList = [bing, ftv, chrcast, spot, earth].map((prov) => prov.provide);
