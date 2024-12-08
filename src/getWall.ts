32a4550a1b55a130033c61b82cfec1e2e2a755f6
spacefall <lucarichu@outlook.it>
2024-12-08 20:15:02 +0100

    fix: allow cors

--- src/getWall.ts
+++ src/getWall.ts
@@ -7,7 +7,7 @@ export async function getWall(url: URL, provs: ProviderMap, env: EnvVars | NodeJ
 		const settings = getSettings(url.searchParams, env, provs);
 		const apiResp = await settings.providers[randInt(settings.providers.length)](settings);
 		return new Response(JSON.stringify(apiResp), {
-			headers: { "Content-Type": "application/json" },
+			headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
 		});
 	} catch (err) {
 		console.error(err);
