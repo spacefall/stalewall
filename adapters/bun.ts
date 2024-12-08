393735fe51ecc689b052d8c3f59a59c6e5a33a92
spacefall <lucarichu@outlook.it>
2024-12-08 17:32:20 +0100

    feat: show cpu time on bun adapter

--- adapters/bun.ts
+++ adapters/bun.ts
@@ -6,14 +6,19 @@ const provs = devLoadProviders();
 const server = Bun.serve({
 	port: 3000,
 	async fetch(req) {
+		const cputime = process.cpuUsage();
 		const url = new URL(req.url);
 
 		// stopping any request not on root from continuing
 		if (url.pathname !== "/") {
 			return new Response("Requested api endpoint does not exist", { status: 404 });
 		}
-
-		return await getWall(url, provs, process.env);
+		const wall = await getWall(url, provs, process.env);
+		const finalcputime = process.cpuUsage(cputime);
+		console.log(
+			`CPU time for request:\nUser: ${finalcputime.user / 1000}ms System: ${finalcputime.system / 1000}ms`,
+		);
+		return wall;
 	},
 });
 
